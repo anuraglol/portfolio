123576ae7173839108e35c0f5a8565c505777383
Anurag <kr.anurag24@gmail.com>
2021-12-07 09:51:47 +0000

    feat: add nextjs-progressbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1>hello world</h1>;
+  return (
+    <div>
+      hello world
+    </div>
+  
+    );
 };
 
 export default Home;
