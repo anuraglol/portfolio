9147cdc6053d79924b8a1341d3fb6c7935ce8807
Anurag <kr.anurag24@gmail.com>
2021-12-07 09:24:59 +0000

    feat: convert to typescript

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <h1>Hello world </h1>
-  )
-}
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <h1>hello world</h1>
+  )
+}
+
+export default Home
