1f6a276ad73a02237693f93ce78435c59b678e44
Anurag <kr.anurag24@gmail.com>
2021-12-07 09:41:25 +0000

    feat: small changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import type { AppProps /*, AppContext */ } from 'next/app'
+import type { AppProps /*, AppContext */ } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <h1>hello world</h1>
-  )
-}
+  return <h1>hello world</h1>;
+};
 
-export default Home
+export default Home;
