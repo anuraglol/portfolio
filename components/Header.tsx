ea5020d0a2c116eeb88f22aa0a1534e8921b64f2
Anurag <kr.anurag24@gmail.com>
2021-12-10 14:34:53 +0000

    enhancement: add fun little hover effects

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Li = ({ text, href }) => {
   return (
     <Link href={href} passHref>
-      <span className="mx-4 hover:text-slate-800 duration-100 cursor-pointer">
+      <span className="mx-4 hover:text-slate-800 hover:rotate-3 duration-100 cursor-pointer">
         {text}
       </span>
     </Link>
@@ -16,7 +16,7 @@ const Header: NextPage = () => {
   return (
     <header className="header h-64 min-w-screen flex items-center flex-row p-10 px-24 pt-20 font-poppins font-medium">
       <p className="flex flex-row items-center mb-24">
-        <span className="text-2xl font-semibold text-secondary">Anurag</span>
+        <span className="text-2xl font-semibold text-secondary duration-100 hover:rotate-12 cursor-pointer">Anurag</span>
 
         <ul className="flex flex-row mx-8 text-lg font-semibold">
           <Li text="Home" href="/" />
