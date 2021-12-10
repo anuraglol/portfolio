e1eb6681298fe7de49972ef075a27300a4a25b2d
Anurag <kr.anurag24@gmail.com>
2021-12-10 14:04:36 +0000

    style: add hover eeffect

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,8 @@
 import type { NextPage } from "next";
-import wave from "../public/assests/wave.svg";
-
-import Image from "next/image";
 
 const Li = ({ text, href }) => {
   return (
-    <a href={href} className="mx-4">
+    <a href={href} className="mx-4 hover:text-slate-800 duration-100">
       {text}
     </a>
   );
@@ -13,7 +10,7 @@ const Li = ({ text, href }) => {
 
 const Header: NextPage = () => {
   return (
-    <header className="header h-64 min-w-screen flex items-center flex-row p-10 px-18 pt-20 font-poppins font-medium">
+    <header className="header h-64 min-w-screen flex items-center flex-row p-10 px-24 pt-20 font-poppins font-medium">
       <p className="flex flex-row items-center mb-24">
         <span className="text-2xl font-semibold text-secondary">Anurag</span>
 
@@ -23,6 +20,10 @@ const Header: NextPage = () => {
           <Li text="Projects" href="/" />
           <Li text="Contact Me" href="/" />
         </ul>
+
+        <button className="p-4 rouned-sm bg-white-300">
+ 
+        </button>
       </p>
     </header>
   );
