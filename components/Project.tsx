a8a4c5659c8a3bee62bce8c88f723375a524a767
Anurag <kr.anurag24@gmail.com>
2021-12-11 18:26:55 +0000

    feat: alomost done

--- components/Project.tsx
+++ components/Project.tsx
@@ -3,8 +3,8 @@ import { BsArrowRightShort } from "react-icons/bs";
 
 const Project =  ({ title, link, role, desc, github }) => {
   return (
-    <div className="my-2 p-4">
-      <a href="#" className="font-medium text-xl hover:text-blue-600">
+    <div className="my-2 p-4 group cursor-pointer">
+      <a href="#" className="font-medium text-xl group-hover:text-blue-600">
         {title}
       </a>
 
@@ -13,7 +13,7 @@ const Project =  ({ title, link, role, desc, github }) => {
         <span className="text-blue-600">role: </span>
         {role}
       </p>
-      <p className="group flex flex-row items-center">
+      <p className="flex flex-row items-center">
         <button className="p-2 rounded-lg bg-gray-100 duration-100 hover:bg-gray-200">
           <AiFillGithub size={25} />
         </button>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,53 +2,58 @@ import type { NextPage } from "next";
 
 const Skills: NextPage = () => {
   return (
-    <div className="mx-24 font-poppins">
-      <p className="font-semibold text-blue-600 text-2xl">/skills</p>
-      <div className="text-gray-500 font-medium">
-        <div className="my-4">
-          <p className="text-gray-600 font-semibold">languages</p>
-          <ul>
-            <li>javascript</li>
-            <li>typescript</li>
-            <li>python</li>
-          </ul>
-        </div>
-        <div className="w-96 grid grid-cols-2 gap-8 my-4">
-          <div className="flex flex-col">
-            <p className="text-gray-600 font-semibold">frameworks</p>
+    <div className="flex flex-row items-center">
+      <div className="mx-24 font-poppins">
+        <p className="font-semibold text-blue-600 text-2xl">/skills</p>
+        <div className="text-gray-500 font-medium">
+          <div className="my-4">
+            <p className="text-gray-600 font-semibold">languages</p>
             <ul>
-              <li>nextjs</li>
+              <li>javascript</li>
+              <li>typescript</li>
+              <li>python</li>
             </ul>
+          </div>
+          <div className="w-96 grid grid-cols-2 gap-8 my-4">
+            <div className="flex flex-col">
+              <p className="text-gray-600 font-semibold">frameworks</p>
+              <ul>
+                <li>nextjs</li>
+              </ul>
+            </div>
+
+            <div>
+              <p className="text-gray-600 font-semibold">libraries</p>
+              <ul>
+                <li>reactjs</li>
+                <li>express js</li>
+                <li>fastapi</li>
+              </ul>
+            </div>
           </div>
 
-          <div>
-            <p className="text-gray-600 font-semibold">libraries</p>
-            <ul>
-              <li>reactjs</li>
-              <li>express js</li>
-              <li>fastapi</li>
-            </ul>
+          <div className="w-96 grid grid-cols-2 my-4 gap-8">
+            <div>
+              <p className="text-gray-600 font-semibold">tools</p>
+              <ul>
+                <li>vscode</li>
+                <li>postman</li>
+                <li>git</li>
+                <li>github</li>
+              </ul>
+            </div>
+            <div>
+              <p className="text-gray-600 font-semibold">css libraries</p>
+              <ul>
+                <li>chakra ui</li>
+                <li>tailwind css</li>
+              </ul>
+            </div>
           </div>
         </div>
-
-        <div className="w-96 grid grid-cols-2 my-4 gap-8">
-          <div>
-            <p className="text-gray-600 font-semibold">tools</p>
-            <ul>
-              <li>vscode</li>
-              <li>postman</li>
-              <li>git</li>
-              <li>github</li>
-            </ul>
-          </div>
-          <div>
-            <p className="text-gray-600 font-semibold">css libraries</p>
-            <ul>
-              <li>chakra ui</li>
-              <li>tailwind css</li>
-            </ul>
-          </div>
-        </div>
+      </div>
+      <div>
+        <img src="./skill.svg" />
       </div>
     </div>
   );
