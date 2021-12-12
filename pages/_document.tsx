20e3ec4d284c4fdc4b8e8cfa27c980da73d0f42d
Anurag <kr.anurag24@gmail.com>
2021-12-12 07:00:22 +0000

    fix: fix accesibility

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head></Head>
         <body className="bg-primary">
           <Main />
