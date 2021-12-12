47b8716d1081430d38afe6dfa736dbc8caa42375
Anurag <kr.anurag24@gmail.com>
2021-12-12 13:59:47 +0000

    feat: add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#FEF6FF",
+        primary: "#FEF7FF",
         secondary: "#845EC2",
       },
     },
