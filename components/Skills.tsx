3d45905e653e64e523fa917795600310aeef63bb
Anurag <kr.anurag24@gmail.com>
2021-12-25 16:01:01 +0000

    feat: add more skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -13,15 +13,24 @@ const Tile = ({ text }) => {
 const Skills = () => {
   return (
     <div className="-mt-12  px-8 py-2 flex justify-center flex-col font-inter font-medium">
-      <p className="text-center text-3xl font-mono font-semibold text-purple-600">skills</p>
+      <p className="text-center text-3xl font-mono font-semibold text-purple-600">
+        skills
+      </p>
       <div className="-mt-16 w-full flex justify-center">
-        <Image src="/assests/illustration.png" height="330" width="330" alt="skills"/>
+        <Image
+          src="/assests/illustration.png"
+          height="330"
+          width="330"
+          alt="skills"
+        />
       </div>
       <div className="w-full flex justify-center gap-8">
         <Tile text="NextJS" />
         <Tile text="Tailwind CSS" />
         <Tile text="Node JS" />
         <Tile text="Supabase" />
+        <Tile text="Typescript" />
+        <Tile text="Figma" />
       </div>
     </div>
   );
