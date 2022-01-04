697e2f00e665d3edee4caf96f1baf996a97eb295
Anurag <kr.anurag24@gmail.com>
2022-01-04 14:42:10 +0000

    fix: add id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script
+      id="google-analytics"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
       />
