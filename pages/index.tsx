9e4ed62f6dcdf5df48a7e8c4133cfbb8592eb80b
Anurag <kr.anurag24@gmail.com>
2022-01-07 14:53:49 +0000

    feat: add back to top button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import type { NextPage } from "next";
+import { HiArrowSmUp } from "react-icons/hi";
+import { useState, useEffect } from "react";
 
 import {
   Header,
@@ -10,6 +12,18 @@ import {
 } from "../components";
 
 const Home: NextPage = () => {
+  const [topOfPage, setTopOfPage] = useState<boolean>();
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 1) {
+        setTopOfPage(false);
+      } else {
+        setTopOfPage(true);
+      }
+    });
+  }, []);
+
   return (
     <>
       <Header />
@@ -18,6 +32,16 @@ const Home: NextPage = () => {
       <Projects />
       <Contact />
       <Footer />
+
+      <button
+        className={`h-14 w-14 m-4 rounded-md bg-arrow text-white font-medium animate-reveal flex items-center justify-center cursor-pointer float-right z-50 shadow-lg hover:shadow-xl duration-100  fixed bottom-3 right-3 ${
+          topOfPage && "hidden"
+        }`}
+        onClick={() => window.scrollTo(0, 0)}
+        aria-label="back-to-top"
+      >
+        <HiArrowSmUp size={50} />
+      </button>
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,22 @@ module.exports = {
         primary: "#FEF7FF",
         secondary: "#845EC2",
         header: "#ffffffd4",
+        arrow: "#000000e4"
       },
 
       fontFamily: {
         inter: "'Inter', sans-serif",
+      },
+
+      animation: {
+        reveal: "reveal 0.1s linear",
+      },
+
+      keyframes: {
+        reveal: {
+          "0%": { bottom: "0", visibility: "hidden" },
+          "100%": { bottom: "1em", visibility: "visible" },
+        },
       },
     },
   },
