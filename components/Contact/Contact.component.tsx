679709320a2ab42e7901df9f6c012beba4743888
Anurag <kr.anurag24@gmail.com>
2022-01-23 07:44:45 +0000

    fix: fix links :P

--- components/Contact/Contact.component.tsx
+++ components/Contact/Contact.component.tsx
@@ -1,23 +1,40 @@
 import { AiOutlineTwitter, AiOutlineGithub } from "react-icons/ai";
 import { MdEmail } from "react-icons/md";
 
+import Link from "next/link";
+
 const Contact = () => {
   return (
     <div className="px-3 font-sen">
       <p className="text-3xl text-white font-bold">Get in touch</p>
 
       <div className="my-8 flex flex-row gap-x-4 justify-center">
-        <button className="text-white text-2xl p-2 rounded-lg bg-zinc-800 border-2 border-zinc-900 hover:border-white">
-          <AiOutlineGithub />
-        </button>
+        <Link href="https://github.com/kr-anurag" passHref>
+          <a
+            target="_blank"
+            className="text-white text-2xl p-2 rounded-lg bg-zinc-800 border-2 border-zinc-900 hover:border-white"
+          >
+            <AiOutlineGithub />
+          </a>
+        </Link>
 
-        <button className="text-white text-2xl p-2 rounded-lg bg-zinc-800 border-2 border-zinc-900 hover:border-white">
-          <AiOutlineTwitter />
-        </button>
+        <Link href="https://twitter.com/kr_anurag_" passHref>
+          <a
+            target="_blank"
+            className="text-white text-2xl p-2 rounded-lg bg-zinc-800 border-2 border-zinc-900 hover:border-white"
+          >
+            <AiOutlineTwitter />
+          </a>
+        </Link>
 
-        <button className="text-white text-2xl p-2 rounded-lg bg-zinc-800 border-2 border-zinc-900 hover:border-white">
-          <MdEmail />
-        </button>
+        <Link href="mailto:kr.anurag24@gmail.com" passHref>
+          <a
+            target="_blank"
+            className="text-white text-2xl p-2 rounded-lg bg-zinc-800 border-2 border-zinc-900 hover:border-white"
+          >
+            <MdEmail />
+          </a>
+        </Link>
       </div>
     </div>
   );
