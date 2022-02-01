da47021cdcd3105ad75cbeef0f6d22c8dae28cea
alceil <ashishshajisrampickal@gmail.com>
2022-02-01 16:41:18 +0530

    Added hamburger menu icon

--- components/Header/Header.component.tsx
+++ components/Header/Header.component.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-
+import { GiHamburgerMenu } from "react-icons/gi";
 import Link from "next/link";
 import { VscGithubAlt } from "react-icons/vsc";
+import { IoClose } from "react-icons/io5";
 
 const TextLink = ({ text, href }: any) => {
   return (
@@ -26,19 +27,33 @@ const Header: React.FC = () => {
       }
     });
   }, []);
+  const [open,setOpen]=useState(false);
 
   return (
     <header
       className={`flex flex-row items-center justify-between py-8 font-jost`}
     >
-      <p className="hidden sm:flex sm:flex-row sm:gap-x-4">
+
+
+  <div >
+  <div onClick={()=>setOpen(!open)} className="md:hidden flex items-center">
+{  open?
+  <IoClose color="white"  size="20px"/>:<GiHamburgerMenu color="white"/>
+  
+  }
+</div>
+      <p className={`${open ? 'flex flex-col':'hidden sm:flex sm:flex-row sm:gap-x-4'}`}>
         <TextLink text="Home" href="#" />
         <TextLink text="Skills" href="#skills" />
         <TextLink text="Projects" href="#projects" />
         <TextLink text="Contact" href="#contact" />
       </p>
+  </div>   
 
-      <Link href="https://github.com/kr-anurag/portfolio" passHref>
+      
+
+
+<Link href="https://github.com/kr-anurag/portfolio" passHref>
         <a
           className="rounded-lg border-2 border-zinc-900 bg-zinc-800 p-2 text-2xl text-white hover:border-white"
           target="_blank"
@@ -48,6 +63,7 @@ const Header: React.FC = () => {
           <VscGithubAlt />
         </a>
       </Link>
+
     </header>
   );
 };
