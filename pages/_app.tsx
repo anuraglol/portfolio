db1c929d52319c30f8bb332ecebb7dfacf2bc0aa
Anurag <kr.anurag24@gmail.com>
2022-02-12 16:12:10 +0000

    chore: remove ga for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
-import Script from "next/script";
 import NextNProgress from "nextjs-progressbar";
 import { NextSeo } from "next-seo";
 import Head from "next/head";
@@ -9,22 +8,7 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script
-        id="google-analytics"
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-      />
 
-      <Script strategy="lazyOnload" id="google-analytics">
-        {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
-      </Script>
       <NextSeo
         title="Anurag | Frontend Developer"
         titleTemplate="Anurag | Frontend Developer"
