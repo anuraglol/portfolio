f01a53e3ca8df4bd10eb9a40201b4a03a5c14f32
Anurag <kr.anurag24@gmail.com>
2022-02-13 04:23:32 +0000

    chore: add google analytics, remove nextnprogressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,28 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
-import NextNProgress from "nextjs-progressbar";
 import { NextSeo } from "next-seo";
 import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
 
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
       <NextSeo
         title="Anurag | Frontend Developer"
         titleTemplate="Anurag | Frontend Developer"
@@ -44,7 +58,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="icon" type="image/png" href="/assests/avatar.png" />
       </Head>
-      <NextNProgress />
       <Component {...pageProps} />
     </>
   );
