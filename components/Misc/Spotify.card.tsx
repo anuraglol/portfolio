ffa421324a2ea290b40e4d48efd793f784c20e77
Anurag <kr.anurag24@gmail.com>
2022-03-31 13:48:23 +0000

    ✨ feat: use swr pkg to fecth data

--- components/Misc/Spotify.card.tsx
+++ components/Misc/Spotify.card.tsx
@@ -1,38 +1,37 @@
-import { FC, useState, useEffect } from "react";
+import { FC } from "react";
 
 import type { NowPlayingSong } from "../../@types/now-playing-song.type";
 import Image from "next/image";
-import axios from "axios";
 import Link from "next/link";
 
+import useSWR from "swr";
+import { fetcher } from "../../lib/fetcher";
+
 const SpotifyCard: FC = () => {
-  const [data, setData] = useState<NowPlayingSong>();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await axios.get("/api/now-playing");
-      setData(res.data);
-    };
-
-    fetchData();
-  }, []);
+  const { data, error } = useSWR<NowPlayingSong>("/api/now-playing", fetcher);
 
   return (
     <>
       <div className="font-sen mb-8 flex flex-row items-center justify-center gap-x-2 rounded-md bg-zinc-800 px-3 py-2 text-center text-lg text-gray-300">
         {data?.isPlaying ? (
           <>
-          <Image src={data?.albumImageUrl} height="70" width="70" className="rounded-md" alt="album url" />
+            <Image
+              src={data?.albumImageUrl}
+              height="70"
+              width="70"
+              className="rounded-md"
+              alt="album url"
+            />
 
-          <Link href={data?.songUrl} passHref>
-            <p className="w-4/5  cursor-pointer">
-              i&apos;m currently listening to{" "}
-              <span className=" text-white">{data?.title}</span>
-              <p>
-                by <span className=" text-white">{data?.artist}</span>
+            <Link href={data?.songUrl} passHref>
+              <p className="w-4/5  cursor-pointer">
+                i&apos;m currently listening to{" "}
+                <span className=" text-white">{data?.title}</span>
+                <p>
+                  by <span className=" text-white">{data?.artist}</span>
+                </p>
               </p>
-            </p>
-          </Link>
+            </Link>
           </>
         ) : (
           <>
