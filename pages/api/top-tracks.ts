325cd2e90af29a5cabd0e1d879d8e85074ce65dc
Anurag <kr.anurag24@gmail.com>
2022-04-17 10:42:57 +0530

    🍻 chore: limit the api res to first 5 items

--- pages/api/top-tracks.ts
+++ pages/api/top-tracks.ts
@@ -8,7 +8,7 @@ export default async function handler(
   const response = await getTopTracks();
   const { items } = await response.json();
 
-  const tracks = items.slice(0, 10).map((track) => ({
+  const tracks = items.slice(0, 5).map((track) => ({
     artist: track.artists.map((_artist) => _artist.name).join(", "),
     songUrl: track.external_urls.spotify,
     title: track.name,
